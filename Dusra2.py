4ffbe6d248dcdf7cbaaa1e8ae6d8827f30372c8f
DIVYANSHUSAINI12 <saini.divyanshu12@gmail.com>
2024-07-29 16:58:11 +0530

    Update Dusra2.py

--- Dusra2.py
+++ Dusra2.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Set up Google Sheets API credentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("waste-ka-audit-b27a6f4efa97.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("waste-ka-audit-c7000141f0a4.json", scope)
 client = gspread.authorize(creds)
 
 # Open the Google Sheet
